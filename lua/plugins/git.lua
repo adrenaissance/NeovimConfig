118b0a0229492970c44ff53ab6ba520ea6dcce71
Alex D'agostino Aleksandrov <alex@adrenaissance.com>
2025-02-16 02:51:16 +0100

    adding new fugitive keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,11 @@
 return {
   "tpope/vim-fugitive",
-  lazy = false,
+  lazy = true,
+  keys = {
+    { "<leader>gb", "<cmd>Git blame<cr>", mode = "n", desc = "Open git blame tree" },
+    { "<leader>gs","<cmd>Git show<cr>", mode = "n", desc = "Git show" },
+    { "<leader>gl","<cmd>Gclog<cr>", mode = "n", desc = "Git log commits" },
+    { "<leader>gdv","<cmd>Gvdiffsplit<cr>", mode = "n", desc = "Git diff vertical split" },
+    { "<leader>gdh","<cmd>Ghdiffsplit<cr>", mode = "n", desc = "Git diff horizontal split" },
+ }
 }
