b7349fb6f151c554cba303c735faff8eb908b9d1
Alex D'agostino Aleksandrov <alex@adrenaissance.com>
2025-02-16 11:46:14 +0100

    adding light-dark theme support

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
+  {
     "n1ghtmare/noirblaze-vim",
     priority = 1000,
     lazy = false,
+  },
+  {
+    "yorickpeterse/vim-paper",
+    priority = 1000,
+    lazy = false,
     config = function() 
-      vim.cmd([[colo noirblaze]])
+      local hour = tonumber(os.date("%H"))
+      if hour > 16 then
+        vim.cmd([[colorscheme noirblaze]])
+      else
+        vim.cmd([[colorscheme paper]])
+      end 
     end,
+  }
 }
